3f34943dd3208790988201f7a774e6e7c6c5ea15
superUserT <rantshothabisomail@gmail.com>
2025-04-10 23:00:32 +0200

    datatype nums

---
+++ datatype_numbers.js
@@ -0,0 +1,21 @@
+const num = 25;
+const floatNum = 15.4;
+
+// num operations
+const sum = num + 5;
+const diff = num - 5;
+const prod = num * 5;
+const quot = num / 5;
+const mod = num % 5;
+
+console.log(sum, diff, prod, quot, mod);
+
+
+// float operations 
+const sumFloat = floatNum  + 5;
+const diffFloat = floatNum  - 5;
+const prodFloat = floatNum * 5;
+const quotFloat = floatNum  / 5;
+const modFloat = floatNum  % 5;
+
+console.log(sumFloat, diffFloat, prodFloat, quotFloat, modFloat);
